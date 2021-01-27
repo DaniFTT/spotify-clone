a68b6d9269b525fd9d3379d532e3b211d1be753b
Daniel Santos De Sousa <64164438+DaniFTT@users.noreply.github.com>
2021-01-26 22:33:49 -0300

    Created the Player component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
-import Player from "./components/Player";
+import Player from "./components/Player/Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-
-  const [token, setToken] = useState(null);
-
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
-
     const _token = hash.access_token;
 
-    if(_token) {
-      setToken(_token);
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
 
       spotify.setAccessToken(_token);
-
-      spotify.getMe().then(user => {
-        console.log(user);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        })
+      })
     }
-
-
-    console.log("i have a token>> ", token);
   }, []);
 
-  return (
-    <div className="app">
-      {
-        token ? (
-          <Player />
-        ) : (
-          <Login />
-        )
-      }
-    </div>
-  );
+  return <div className="app">{token ? <Player spotify={spotify} /> : <Login />}</div>;
 }
 
 export default App;
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,9 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
+  // remove token after developing
+  token:
+    "BQD-M87verh4gKBbLrmpaugjyQBLj1RPytRn1eU2jSysA_vK-N530kf5QRv-I7xD3F4ISxmG6fq8LLjILqF7H9kgKy5bcsmQAqhpMa7ooccj5rGA0ilC_-HcEcxOmL_T-TavTFpsqjQOQQmPmDlq7iXKe0nWT9V6YLulJGLAqwt9KpNHwugP",
 };
 
 const reducer = (state, action) => {
@@ -10,16 +13,25 @@ const reducer = (state, action) => {
 
   // action  --> type, [payload]
 
-  switch(action.type){
-    case 'SET_USER':
+  switch (action.type) {
+    case "SET_USER":
       return {
-        ...state, 
-        user: action.user
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists
       }
-    default: 
+    default:
       return state;
   }
-
-}
+};
 
 export default reducer;
